d17741d66b214de3f48ce1ac3885d1cf21426287
BenjiCCB <bensdecker+CCB@gmail.com>
2023-01-25 20:30:35 -0500

    more tests

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -7,6 +7,11 @@ describe("Employee", () => {
             const obj = new Employee(type, name, id, email, field3);
 
             expect("type" in obj).toEqual(true);
+            expect("name" in obj).toEqual(true);
+            expect("id" in obj).toEqual(true);
+            expect("email" in obj).toEqual(true);
+            expect("field3" in obj).toEqual(true);
+            
         });
     });
 
